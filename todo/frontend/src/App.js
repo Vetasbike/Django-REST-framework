fb75c2a095ef189c0763b55d9c15fb1ce29abe02
Vitaly <vetasbike@mail.ru>
2023-03-02 11:16:29 +0300

    All done lesson 7

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+import logo, {ReactComponent as ReactLogo} from './logo.svg';
 import './App.css';
 import React from "react";
 import axios from "axios";
-import {Container} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 import UserList from './components/User';
@@ -10,7 +10,9 @@ import ProjectList from './components/Project';
 import ProjectTodosList from './components/ProjectTodos';
 import TodoList from './components/Todo';
 import NotFound404 from "./components/NotFound404";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import LoginForm from "./components/Auth";
+import Cookies from "universal-cookie";
 
 
 
@@ -20,12 +22,55 @@ class App extends React.Component {
         this.state = {
             'users': [],
             'projects': [],
-            'todos': []
+            'todos': [],
+            'token': '',
         }
     }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users')
+    get_token(username, password) {
+        const data = {username: username, password:password}
+        axios.post('http://127.0.0.1:8000/api-token/', data)
+            .then(response => {this.set_token(response.data['token'])})
+            .catch(error => alert('Неверный логин или пароль'))
+    }
+
+    set_token(token) {
+        console.log(token)
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, () => this.load_data())
+    }
+
+    is_auth() {
+        return !!this.state.token
+    }
+
+    logout() {
+        this.set_token('')
+        this.setState({'users': []}, () => this.load_data())
+        this.setState({'projects': []}, () => this.load_data())
+        this.setState({'todos': []}, () => this.load_data())
+    }
+
+    get_headers() {
+        let headers = {
+            'Content-Type': 'applications/json'
+        }
+        if (this.is_auth()) {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, () => this.load_data())
+    }
+
+    load_data() {
+        const headers = this.get_headers()
+        axios.get('http://127.0.0.1:8000/api/users', {headers})
             .then(response => {
                 const users = response.data
                 this.setState(
@@ -34,7 +79,7 @@ class App extends React.Component {
                     }
                 )
             }).catch(error => console.log(error))
-        axios.get('http://127.0.0.1:8000/api/projects')
+        axios.get('http://127.0.0.1:8000/api/projects', {headers})
             .then(response => {
                 const projects = response.data
                 this.setState(
@@ -43,7 +88,7 @@ class App extends React.Component {
                     }
                 )
             }).catch(error => console.log(error))
-        axios.get('http://127.0.0.1:8000/api/todos')
+        axios.get('http://127.0.0.1:8000/api/todos', {headers})
             .then(response => {
                 const todos = response.data
                 this.setState(
@@ -54,18 +99,40 @@ class App extends React.Component {
             }).catch(error => console.log(error))
     }
 
+    componentDidMount() {
+        this.get_token_from_storage()
+    }
+
     render() {
         return (
+
         <Container className="p-2">
             <Container className="p-5 mb-4 bg-light rounded-3">
                 <BrowserRouter>
-                    <Menu />
+                    {/*<Menu /> */}
+                    <Navbar bg="dark" variant="dark">
+                        <Container>
+                            <ReactLogo width="30" height="30" />
+                                <Nav className="me-auto">
+                                    <Nav.Link as={Link} to="/">Пользователи</Nav.Link>
+                                    <Nav.Link as={Link} to="/projects">Проекты</Nav.Link>
+                                    <Nav.Link as={Link} to="/todos">Todo</Nav.Link>
+                                    {/*<Nav.Link as={Link} to="/login">Вход</Nav.Link>*/}
+                                    {this.is_auth() ?
+                                        <Nav.Link onClick={()=>this.logout()}>Выход</Nav.Link> :
+                                        <Nav.Link as={Link} to="/login">Вход</Nav.Link>}
+                                </Nav>
+                        </Container>
+                    </Navbar>
                         <Routes>
                             <Route exact path='/' element={<UserList users={this.state.users} />} />
                             <Route exact path='/projects' element={<ProjectList projects={this.state.projects} />} />
                             <Route exact path='/todos' element={<TodoList todos={this.state.todos} />} />
                             <Route path='/projects/:id' element={<ProjectTodosList todos={this.state.todos} />} />
                             <Route exact path='/users' element={<UserList users={this.state.users} />} />
+                            <Route exact path='/login' element={
+                                <LoginForm get_token={(username, password) => this.get_token(username, password)
+                                } />} />
                             <Route path='*' element={<NotFound404 />} />
                         </Routes>
                     <Footer />
--- todo/todoapp/models.py
+++ todo/todoapp/models.py
@@ -33,3 +33,4 @@ class Todo(models.Model):
 
     def __str__(self):
         return f'{self.text}'
+    
