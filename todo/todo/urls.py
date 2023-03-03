ee63c2f23bf1111ec8d7c4a6df684a83cd21d07e
Vitaly <vetasbike@mail.ru>
2023-03-03 12:25:27 +0300

    All done lesson 8

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -29,7 +29,7 @@ router.register('todos', TodoModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-token/', obtain_auth_token),
+    path('api-token/', obtain_auth_token, name='token'),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
